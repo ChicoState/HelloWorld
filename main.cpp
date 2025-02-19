9958372e68bcc57f77e69266d3b85edebcdb3c65
ezaz <eamohammadabdul@csuchico.edu>
2025-02-19 11:37:17 -0800

    added docker

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main(){
-  std::cout<<"Hello World!\n";
-  return 0;
+int main() {
+    std::vector<std::string> favoriteFoods;
+    std::string input;
+    
+    std::cout << "Enter your favorite food one by one. Type 'done' when finished.\n";
+    
+    while (true) {
+        std::cout << "What's your favorite food? ";
+        std::getline(std::cin, input);
+        
+        // Check if the user is done entering foods
+        if (input == "done") {
+            break;
+        }
+        
+        // Add the food to the list
+        favoriteFoods.push_back(input);
+    }
+    
+    // Print the list of favorite foods
+    std::cout << "\nYour favorite foods are:\n";
+    for (const auto& food : favoriteFoods) {
+        std::cout << "- " << food << "\n";
+    }
+    
+    return 0;
 }
+
